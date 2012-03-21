c29c4a5890a9c9cd380dcbbdc09626b4b9e2b718
Eneko <nkogear@gmail.com>
2012-03-21 15:18:23 +0100

    MazeProblem implemented (operator creation and solve() method)

--- src/es/deusto/ingenieria/maze/MazeProblem.java
+++ src/es/deusto/ingenieria/maze/MazeProblem.java
@@ -1,22 +1,45 @@
 package es.deusto.ingenieria.maze;
 
+import es.deusto.ingenieria.aike.formulation.Operator;
 import es.deusto.ingenieria.aike.formulation.Problem;
+import es.deusto.ingenieria.aike.search.Node;
+import es.deusto.ingenieria.aike.search.SearchMethod;
+import es.deusto.ingenieria.maze.MoveOperator.Direction;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MazeProblem extends Problem {
 
-	public MazeProblem() {
-		// TODO Auto-generated constructor stub
-	}
+    public MazeProblem(Object initialEnvironment, Object finalEnvironment) {
+        super(initialEnvironment, finalEnvironment);
+        createOperators();
+    }
 
-	public MazeProblem(Object initialEnvironment, Object finalEnvironment) {
-		super(initialEnvironment, finalEnvironment);
-		// TODO Auto-generated constructor stub
-	}
+    @Override
+    protected void createOperators() {
+        Operator opMoveUp = new MoveOperator(Direction.UP);
+        this.addOperator(opMoveUp);
+        Operator opMoveDown = new MoveOperator(Direction.DOWN);
+        this.addOperator(opMoveDown);
+        Operator opMoveLeft = new MoveOperator(Direction.LEFT);
+        this.addOperator(opMoveLeft);
+        Operator opMoveRight = new MoveOperator(Direction.RIGHT);
+        this.addOperator(opMoveRight);
+    }
+    
+    public List<String> solve(SearchMethod searchMethod) {
+        Node finalNode = searchMethod.search(this, this.getInitialStates().get(0));
 
-	@Override
-	protected void createOperators() {
-		// TODO Auto-generated method stub
-
-	}
+        if (finalNode != null) {
+            System.out.println("Solution found!!");
+            List<String> operators = new ArrayList<String>();
+            searchMethod.solutionPath(finalNode, operators);
+            searchMethod.createSolutionLog(operators);
+            return operators;
+        } else {
+            System.out.println("Unable to find the solution!");
+            return null;
+        }
+    }
 
 }
