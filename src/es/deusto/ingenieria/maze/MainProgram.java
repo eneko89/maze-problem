f848067a8dc5bd4e2cc6c98444eb8aa9a74b8870
eneko <eneko@eneko-pc>
2012-03-24 04:48:21 +0100

    Further implementation of the GUI.

--- src/es/deusto/ingenieria/maze/MainProgram.java
+++ src/es/deusto/ingenieria/maze/MainProgram.java
@@ -2,7 +2,17 @@ package es.deusto.ingenieria.maze;
 
 import es.deusto.ingenieria.aike.formulation.State;
 import es.deusto.ingenieria.aike.search.heuristic.BestFS;
+import es.deusto.ingenieria.maze.Cell.Foot;
+import gail.grid.Grid;
+import gail.grid.GridElement;
+import gail.grid.Resources;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
 import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class MainProgram {
 
@@ -22,14 +32,47 @@ public class MainProgram {
             for(String str : operators) {
                 System.out.println(str);
             }
-            createAndShowGUI();
+            createAndShowGUI(initialEnvironment);
         } else {
             System.out.println("Unable to find the solution!");
         }
     }
     
-    public static void createAndShowGUI() {
+    public static void createAndShowGUI(Environment env) {
         
+        /* * * * * * * * * * *
+         * Creating the GUI  *
+         * * * * * * * * * * */
+
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setSize(new Dimension(400, 200));
+
+        // Create the grid.
+        Grid grid = new Grid(env.getColumnCount(), env.getRowCount());
+        grid.setBackground(Color.white);
+        grid.setForeground(Color.lightGray);
+        f.setContentPane(grid);
+        f.setVisible(true);
+        
+        // Loading feet images
+        BufferedImage leftFoot= Resources.loadImageResource(
+                                        "/es/deusto/ingenieria/maze/left.png");
+        BufferedImage rightFoot= Resources.loadImageResource(
+                                        "/es/deusto/ingenieria/maze/right.png");
+        
+        // Add the feet elements.
+        Cell[][] cells = env.getCells();
+        int columns = env.getColumnCount();
+        int rows = env.getRowCount();
+        for(int i=0; i < columns; i++) {
+            for (int j=0; j < rows; j++) {
+                if (cells[i][j].getFoot() == Foot.LEFT)
+                    grid.add(new GridElement(leftFoot), new Point(i,j));
+                else
+                    grid.add(new GridElement(rightFoot), new Point(i,j));
+            }
+        }
     }
 
 }
